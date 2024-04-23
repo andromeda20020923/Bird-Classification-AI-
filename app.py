b6758974efd79472e399751420aff2e0fe03d8cc
Jaxongir <fanaf91318@gmail.com>
2024-04-24 01:43:11 +0500

    uploaded

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp= pathlib.PosixPath
+pathlib.PosixPath=pathlib.WindowsPath
+
+st.title('Qushlarni Klassifikatsiya qiluvchi model')
+file=st.file_uploader('Rasmni yuklash',type=['png','jpg','jpeg','gif','svg'])
+if file:
+    st.image(file)
+    model=load_learner('Birds_classification_model.pkl')
+    img=PILImage.create(file)
+    pred,pred_id,probs=model.predict(img)
+    st.success(f'Bashorat : {pred}')
+    st.info(f'Ehtimollik : {probs[pred_id]*100:.1f}')
+    fig=px.bar(x=probs*100,y=model.dls.vocab)
+    st.plotly_chart(fig)
